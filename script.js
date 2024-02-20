3e53797885f3b23562ad0c30a75b27bfcde80eb2
rinisha1234 <160553196+rinisha1234@users.noreply.github.com>
2024-02-20 14:07:17 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,46 +1,25 @@
-document.addEventListener('DOMContentLoaded', function() {
-    function getQueryParam(name) {
-        const urlParams = new URLSearchParams(window.location.search);
-        return urlParams.get(name);
-    }
+document.getElementById('dataForm').addEventListener('submit', function(e) {
+    e.preventDefault();
 
-    function populateInputField() {
-        const alertParam = getQueryParam('alert');
-        if (alertParam) {
-            const decodedAlert = decodeURIComponent(alertParam);
-            const inputDataField = document.getElementById('inputData');
-            inputDataField.value = decodedAlert; // Set the JSON string as value
-            // Remove the call to adjustTextareaSize
-        }
-    }
+    const inputData = document.getElementById('inputData').value;
+    const severityScore = document.getElementById('severityScore').value;
+    const scoredBy = document.getElementById('scoredBy').value; // Get scored by value
 
-    populateInputField();
+    const apiUrl = 'https://1ne4g6lm7e.execute-api.us-east-1.amazonaws.com/aiops/hello';
 
-    document.getElementById('inputData').addEventListener('input', function() {
-        // Adjust size on user input
-    });
-
-    document.getElementById('dataForm').addEventListener('submit', function(e) {
-        e.preventDefault();
-
-        const inputData = document.getElementById('inputData').value;
-        const severityScore = document.getElementById('severityScore').value; // Get severity score value
-        const apiUrl = 'https://1ne4g6lm7e.execute-api.us-east-1.amazonaws.com/aiops/hello';
-
-        fetch(apiUrl, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({ alert: inputData, severity_score: severityScore }) // Include severity score in payload
-        })
-        .then(response => response.json())
-        .then(data => {
-            document.getElementById('apiResponse').textContent = JSON.stringify(data, null, 2);
-        })
-        .catch(error => {
-            console.error('Error:', error);
-            document.getElementById('apiResponse').textContent = 'Failed to fetch';
-        });
+    fetch(apiUrl, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ alert: inputData, severity_score: severityScore, scored_by: scoredBy }) // Include scored by in payload
+    })
+    .then(response => response.json())
+    .then(data => {
+        document.getElementById('apiResponse').textContent = JSON.stringify(data, null, 2);
+    })
+    .catch(error => {
+        console.error('Error:', error);
+        document.getElementById('apiResponse').textContent = 'Failed to fetch';
     });
 });
