42d8da72f9c05fc1f143f3e71774db4a7f7ceb60
rinisha1234 <160553196+rinisha1234@users.noreply.github.com>
2024-02-20 13:15:00 -0500

    Update script.js

--- script.js
+++ script.js
@@ -4,29 +4,16 @@ document.addEventListener('DOMContentLoaded', function() {
         return urlParams.get(name);
     }
 
-    function adjustTextareaSize(textarea) {
-        textarea.style.height = 'auto'; // Reset height to recalculate
-        textarea.style.width = 'auto'; // Reset width to recalculate
-        textarea.style.resize = 'both'; // Enable resizing both horizontally and vertically
-        textarea.style.height = textarea.scrollHeight + 'px'; // Set new height based on scroll height
-        textarea.style.width = textarea.scrollWidth + 'px'; // Set new width based on scroll width
-    }
-
     function populateInputField() {
         const alertParam = getQueryParam('alert');
         if (alertParam) {
             const decodedAlert = decodeURIComponent(alertParam);
             const inputDataField = document.getElementById('inputData');
             inputDataField.value = decodedAlert; // Set the JSON string as value
-            adjustTextareaSize(inputDataField); // Adjust the textarea size based on its content
         }
     }
 
     populateInputField();
-
-    document.getElementById('inputData').addEventListener('input', function() {
-        adjustTextareaSize(this); // Adjust size on user input
-    });
 
     document.getElementById('dataForm').addEventListener('submit', function(e) {
         e.preventDefault();
@@ -39,7 +26,7 @@ document.addEventListener('DOMContentLoaded', function() {
             headers: {
                 'Content-Type': 'application/json',
             },
-            body: JSON.stringify({ alert: inputData }) // Assuming you want to wrap the inputData in an object with key 'alert'
+            body: JSON.stringify({ key: inputData })
         })
         .then(response => response.json())
         .then(data => {
